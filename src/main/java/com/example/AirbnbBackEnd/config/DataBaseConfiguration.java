7aa0626b660c935893f70dfbb15764844413c7ee
OualidCh11 <118737452+OualidCh11@users.noreply.github.com>
2024-07-31 11:12:31 +0100

    AB-v2

--- src/main/java/com/example/AirbnbBackEnd/config/DataBaseConfiguration.java
+++ src/main/java/com/example/AirbnbBackEnd/config/DataBaseConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories({"com.example.AirbnbBackEnd",})
+@EnableJpaRepositories({"com.example.AirbnbBackEnd.repository"})
 @EnableTransactionManagement
 @EnableJpaAuditing
 public class DataBaseConfiguration {
--- src/main/java/com/example/AirbnbBackEnd/repository/ListingDao.java
+++ src/main/java/com/example/AirbnbBackEnd/repository/ListingDao.java
@@ -1,4 +1,8 @@
 package com.example.AirbnbBackEnd.repository;
 
-public interface ListingDao {
+import com.example.AirbnbBackEnd.entity.Listing;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ListingDao extends JpaRepository<Listing,Long>{
 }
