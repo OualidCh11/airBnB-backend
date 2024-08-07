662e73ae1c4a8bca04c754a50a57ac544ebc3f2d
OualidCh11 <118737452+OualidCh11@users.noreply.github.com>
2024-08-07 14:33:24 +0100

    AB-add security configuration v1

--- src/main/java/com/example/AirbnbBackEnd/config/SecurityConfiguration.java
+++ src/main/java/com/example/AirbnbBackEnd/config/SecurityConfiguration.java
@@ -1,4 +1,33 @@
 package com.example.AirbnbBackEnd.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestResolver;
+
+@Configuration
+@EnableMethodSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception{
+        CsrfTokenRequestAttributeHandler requestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        requestAttributeHandler.setCsrfRequestAttributeName(null);
+        httpSecurity.authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.GET,"api/tenant-listing/get-all-by-category").permitAll()
+                .requestMatchers(HttpMethod.GET, "api/tenant-listing/get-one").permitAll()
+                .requestMatchers(HttpMethod.POST, "api/tenant-listing/search").permitAll()
+                .requestMatchers(HttpMethod.GET, "api/booking/check-availability").permitAll()
+                .requestMatchers(HttpMethod.GET, "assets/*").permitAll()
+                .anyRequest().authenticated()).csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrfTokenRequestHandler(requestAttributeHandler)).oauth2Login(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2Client(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
 }
